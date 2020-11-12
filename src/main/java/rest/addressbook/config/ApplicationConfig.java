91798af97cd136d3801dce4532db22d0846292d0
Alvarogd6 <760704@unizar.es>
2020-11-12 16:09:15 +0100

    apply CORS

--- src/main/java/rest/addressbook/config/ApplicationConfig.java
+++ src/main/java/rest/addressbook/config/ApplicationConfig.java
@@ -4,6 +4,7 @@ import org.eclipse.persistence.jaxb.rs.MOXyJsonProvider;
 import org.glassfish.jersey.internal.inject.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
 import rest.addressbook.domain.AddressBook;
+import rest.addressbook.filter.CorsFilter;
 import rest.addressbook.web.AddressBookController;
 
 public class ApplicationConfig extends ResourceConfig {
@@ -24,6 +25,7 @@ public class ApplicationConfig extends ResourceConfig {
   public ApplicationConfig(final AddressBook addressBook) {
     register(AddressBookController.class);
     register(MOXyJsonProvider.class);
+    register(CorsFilter.class);
     register(new AbstractBinder() {
 
       @Override
